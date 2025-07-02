36f3ce260d9d39b9463a1b577f15885e9a2fa646
qkauia-guy <kauia407@gmail.com>
2025-07-02 11:30:00 +0800

    create QuestionRecord

--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -15,10 +16,24 @@ class Question(models.Model):
     choice_h = models.TextField(default="X")
     answer = models.CharField(max_length=20)  # 多個答案可用逗號分隔
     explanation = models.TextField(blank=True, default="")
-    require_order = models.BooleanField(default=False)  # ✅ 是否順序正確
-    is_fill_in = models.BooleanField(default=False)  # ✅ 是否為填空選擇題
-    fill_answer = models.CharField(max_length=30, blank=True, default="")  # 填空題答案
-    category = models.CharField(max_length=50, default="Python")  # 新增欄位：題庫分類
+    require_order = models.BooleanField(default=False)
+    is_fill_in = models.BooleanField(default=False)
+    fill_answer = models.CharField(max_length=30, blank=True, default="")
+    category = models.CharField(max_length=50, default="Python")
 
     def __str__(self):
         return f"{self.chapter}-{self.number}"
+
+
+class QuestionRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    session_key = models.CharField(max_length=40, null=True, blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    answered_at = models.DateTimeField(auto_now_add=True)
+    selected_answer = models.CharField(max_length=50, blank=True)
+    used_time = models.IntegerField(default=0)
+
+    def __str__(self):
+        who = self.user.username if self.user else f"Session-{self.session_key}"
+        return f"{who} - {self.question} - {'✔' if self.is_correct else '✘'}"
